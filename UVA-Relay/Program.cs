73d5d65ba6bb2b0d878a4df56c8c681f0df7129a
Daniel Nguyen <zeromsv@gmail.com>
2024-04-15 15:42:13 -0500

    Added toml config to program

--- UVA-Relay/Program.cs
+++ UVA-Relay/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Tomlet;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
@@ -7,19 +9,23 @@ using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Enums;
 using DSharpPlus.Interactivity.Extensions;
 using DSharpPlus.SlashCommands;
+using Tomlet.Models;
 using UVACanvasAccess.ApiParts;
-//TODO 
-// INSTALL TOML LIBRARY Tomlyn
+using UVACanvasAccess.Util;
 
 namespace UVA_Relay {
     internal static class Program {
-        public static async Task Main() {
-            Canvas.CanvasApi = new Api(Environment.GetEnvironmentVariable("UVA_RELAY_CANVAS_TOKEN"),
-                                       "https://uview.instructure.com/api/v1/");
+        public static async Task Main()
+        {
+            // Get tokens from toml file
+            TomlDocument document = TomlParser.ParseFile(@Environment.GetEnvironmentVariable("config"));
+            // document.GetSubTable("keys").GetString("BOT_TOKEN"));
+            Canvas.CanvasApi = new Api(document.GetSubTable("keys").GetString("UVA_RELAY_CANVAS_TOKEN"),
+                "https://uview.instructure.com/api/v1/");
             
             var discord = new DiscordClient(new DiscordConfiguration {
                 //Token = Environment.GetEnvironmentVariable("UVA_RELAY_TOKEN") ?? throw new NullReferenceException("UVA_RELAY_TOKEN is unset"),
-                Token = Environment.GetEnvironmentVariable("BOT_TOKEN"),
+                Token = document.GetSubTable("keys").GetString("BOT_TOKEN"),
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.All
             });
@@ -33,7 +39,7 @@ namespace UVA_Relay {
             var testGuildId = ulong.Parse(Environment.GetEnvironmentVariable("UVA_RELAY_TEST_GUILD_ID")
                                           ?? throw new NullReferenceException("UVA_RELAY_TEST_GUILD_ID is unset"));
             */
-            var testGuildId = ulong.Parse(Environment.GetEnvironmentVariable("GUILD_ID")
+            var testGuildId = ulong.Parse(document.GetSubTable("keys").GetString("GUILD_ID")
                                           ?? throw new NullReferenceException("UVA_RELAY_TEST_GUILD_ID is unset"));
             var slash = discord.UseSlashCommands();
             
